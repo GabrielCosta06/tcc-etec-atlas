01cc612acff81de78e54f6b395909ab8c5b0e0dc
José Gabriel Costa <josetropilo@gmail.com>
2023-10-19 23:12:19 -0300

    FIX

--- codigos/Login/index.js
+++ codigos/Login/index.js
@@ -1,3 +1,163 @@
-function irRegistrar() {
-    window.location.href = "./pages/register/register.php";
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        window.location.href = "pages/home/home-logado.html";
+    }
+})
+
+function onChangeEmail() {
+    toggleButtonsDisable();
+    toggleEmailErrors();
+}
+
+function onChangePassword() {
+    toggleButtonsDisable();
+    togglePasswordErrors();
+}
+
+function login() {
+    showLoading();
+    firebase.auth().signInWithEmailAndPassword(
+        form.email().value, form.password().value
+    ).then(() => {
+        hideLoading();
+        window.location.href = "pages/home/home-logado.html";
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    });
+}
+
+function onChangeEmailpt() {
+    toggleButtonsDisable();
+    toggleEmailErrors();
+}
+
+function onChangePasswordpt() {
+    toggleButtonsDisable();
+    togglePasswordErrors();
+}
+
+function loginpt() {
+    showLoading();
+    firebase.auth().signInWithEmailAndPassword(
+        form.email().value, form.password().value
+    ).then(() => {
+        hideLoading();
+        window.location.href = "pages/home/home-logado.html";
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    });
+}
+
+function register() {
+    window.location.href = "pages/register/register.html";
+}
+
+function telaInicial() {
+    if (true) {
+        window.location.href = "../Inicio/home.html"
+    }
+}
+
+function recoverPassword() {
+    showLoading();
+    firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
+        hideLoading();
+        alert('Email enviado com sucesso');
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    });
+}
+
+function getErrorMessage(error) {
+    if (error.code == "auth/user-not-found") {
+        return "Usuário nao encontrado";
+    }
+    if (error.code == "auth/wrong-password") {
+        return "Senha inválida";
+    }
+    return error.message;
+}
+
+function toggleEmailErrors() {
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+    
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+}
+
+function togglePasswordErrors() {
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+}
+
+function toggleButtonsDisable() {
+    const emailValid = isEmailValid();
+    form.recoverPasswordButton().disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    form.loginButton().disabled = !emailValid || !passwordValid;
+}
+
+function isEmailValid() {
+    const email = form.email().value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function isPasswordValid() {
+    return form.password().value ? true : false;
+}
+
+const form = {
+    email: () => document.getElementById("email"),
+    emailInvalidError: () => document.getElementById("email-invalid-error"),
+    emailRequiredError: () => document.getElementById("email-required-error"),
+    loginButton: () => document.getElementById("login-button"),
+    password: () => document.getElementById("password"),
+    passwordRequiredError: () => document.getElementById("password-required-error"),
+    recoverPasswordButton: () => document.getElementById("recover-password-button"),
+} 
+
+function changeLanguage(language) {
+    const englishContent = document.querySelector('.english-content');
+    const portugueseContent = document.querySelector('.portuguese-content');
+
+    if (language === 'en') {
+        englishContent.style.display = 'block';
+        portugueseContent.style.display = 'none';
+    } else if (language === 'pt') {
+        englishContent.style.display = 'none';
+        portugueseContent.style.display = 'block';
+    }
+}
+
+const firestore = firebase.firestore();
+
+function saveGameProgress(userId, gameProgress) {
+    firestore.collection('userProgress').doc(userId).set({
+        progress: gameProgress
+    }).then(() => {
+        console.log('Progresso do jogo salvo com sucesso!');
+    }).catch((error) => {
+        console.error('Erro ao salvar o progresso do jogo: ', error);
+    });
+}
+
+function getGameProgress(userId) {
+    const docRef = firestore.collection('userProgress').doc(userId);
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            const progressData = doc.data();
+            console.log('Progresso do jogo encontrado: ', progressData);
+        } else {
+            console.log('Nenhum progresso do jogo encontrado para o usuário');
+        }
+    }).catch((error) => {
+        console.error('Erro ao obter o progresso do jogo: ', error);
+    });
 }
